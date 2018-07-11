e18f9c7c8167086612734e540522403041fae911
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-07-11 09:13:58 -0300

    Working on the binary search algorithm.

---
+++ udemy/DSAndA.Test/BinarySearch.Test.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace DSAndA.Test
+{
+    public class BinarySearchTestFixture
+    {
+        [Fact]
+        public void BinarySearchTest1()
+        {
+            int[] orderedNumbers = new int[] { 3, 7 };
+
+            Assert.Equal(expected: 0, actual: new BinarySearch(orderedNumbers).Find(3));
+        }
+
+        [Fact]
+        public void BinarySearchTest2()
+        {
+            int[] orderedNumbers = new int[] { 3, 7 };
+
+            Assert.Equal(expected: 1, actual: new BinarySearch(orderedNumbers).Find(7));
+        }
+    }
+}
---
+++ udemy/DSAndA/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace DSAndA
+{
+    public class BinarySearch
+    {
+        private int[] orderedNumbers;
+
+        public BinarySearch(int[] orderedNumbers)
+        {
+            this.orderedNumbers = orderedNumbers;
+        }
+
+        public int Find(int number)
+        {
+            int min = 0;
+            int max = (this.orderedNumbers.Length - 1);
+            int middle = (max + min) / 2;
+
+            if (this.orderedNumbers[middle] == number)
+                return middle;
+            else
+                //Actually, I should keep looking for...
+                return middle + 1;
+        }
+    }
+}
