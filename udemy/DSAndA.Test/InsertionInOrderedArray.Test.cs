df9ec0fdd1eb644de0f54e9777d279c1bbe3588a
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-08-15 09:28:17 -0300

    Adding test for the insertion in an ordered array algorithm.

--- udemy/DSAndA.Test/InsertionInOrderedArray.Test.cs
+++ udemy/DSAndA.Test/InsertionInOrderedArray.Test.cs
@@ -12,5 +12,14 @@ namespace DSAndA.Test
                 actual: new InsertionInOrderedArray().Insert(orderedArray: new int[] { 3, 7 }, itemToInsert: 5)
             );
         }
+
+        [Fact]
+        public void InsertionInOrderedArrayTest2()
+        {
+            Assert.Equal(
+                expected: new int[] { 3, 7, 20, 32 },
+                actual: new InsertionInOrderedArray().Insert(orderedArray: new int[] { 3, 7, 32 }, itemToInsert: 20)
+            );
+        }
     }
 }
--- udemy/DSAndA/InsertionInOrderedArray.cs
+++ udemy/DSAndA/InsertionInOrderedArray.cs
@@ -4,7 +4,43 @@
     {
         public int[] Insert(int[] orderedArray, int itemToInsert)
         {
-            return new int[] { 3, 5, 7 };
+            //Arrays in C# are inmutable, so I have to create a new array but with one slot more than the original one...
+            int[] newOrderedArray = this.GetCopyOfArrayWithFreeSlotAtEnd(orderedArray);
+
+            int positionWhereItemShouldBeInserted = this.GetPositionWhereItemShouldBeInserted(itemToInsert, newOrderedArray);
+
+            //Making space to insert the item...
+            int lastElementPosition = orderedArray.Length - 1;
+            int currentPosition = lastElementPosition;
+
+            do
+            {
+                newOrderedArray[currentPosition + 1] = newOrderedArray[currentPosition--];
+            } while (currentPosition == positionWhereItemShouldBeInserted);
+
+            newOrderedArray[positionWhereItemShouldBeInserted] = itemToInsert;
+
+            return newOrderedArray;
+        }
+
+        private int[] GetCopyOfArrayWithFreeSlotAtEnd(int[] orderedArray)
+        {
+            int[] sameOrderedArrayButWithOneSlotMore = new int[orderedArray.Length + 1];
+
+            for (int i = 0; i < orderedArray.Length; i++)
+                sameOrderedArrayButWithOneSlotMore[i] = orderedArray[i];
+
+            return sameOrderedArrayButWithOneSlotMore;
+        }
+
+        private int GetPositionWhereItemShouldBeInserted(int itemToInsert, int[] newOrderedArray)
+        {
+            int positionWhereItemShouldBeInserted = 0;
+
+            while (itemToInsert >= newOrderedArray[positionWhereItemShouldBeInserted])
+                positionWhereItemShouldBeInserted++;
+
+            return positionWhereItemShouldBeInserted;
         }
     }
 }
