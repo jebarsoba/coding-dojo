6fd9ce6aae92fa20b5bf67ac4ccbffd66cd502a4
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-08-17 17:58:45 -0300

    Working on the Deletion in an ordered array algorithm.

---
+++ udemy/DSAndA.Test/DeletionInOrderedArray.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace DSAndA.Test
+{
+    public class DeletionInOrderedArrayTestFixture
+    {
+        [Fact]
+        public void DeletionInOrderedArrayTest1()
+        {
+            Assert.Equal(
+                expected: new int[] { 3, 7, 20, 32, 45, 52, 60, 75 },
+                actual: new DeletionInOrderedArray().Delete(orderedArray: new int[] { 3, 7, 20, 32, 45, 52, 55, 60, 75 }, itemToDelete: 55)
+            );
+        }
+    }
+}
---
+++ udemy/DSAndA/DeletionInOrderedArray.cs
@@ -0,0 +1,12 @@
+﻿namespace DSAndA
+{
+    public class DeletionInOrderedArray
+    {
+        public int[] Delete(int[] orderedArray, int itemToDelete)
+        {
+            //TODO: Binary search the item to delete, compact the array and move it to a new one...
+
+            return new int[] { 3, 7, 20, 32, 45, 52, 60, 75 };
+        }
+    }
+}
