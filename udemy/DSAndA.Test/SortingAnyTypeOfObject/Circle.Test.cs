b433d4863f55c146b0982d4df7eaace87a6b75c0
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-08-23 10:05:32 -0300

    Working on sorting any type of object.

---
+++ udemy/DSAndA.Test/SortingAnyTypeOfObject/Circle.Test.cs
@@ -0,0 +1,14 @@
+﻿using DSAndA.SortingAnyTypeOfObject;
+using Xunit;
+
+namespace DSAndA.Test.SortingAnyTypeOfObject
+{
+    public class CircleTestFixture
+    {
+        [Fact]
+        public void GreaterThanTest()
+        {
+            Assert.True(new Circle(1.0) > new Circle(0.5));
+        }
+    }
+}
---
+++ udemy/DSAndA/SortingAnyTypeOfObject/Circle.cs
@@ -0,0 +1,22 @@
+﻿namespace DSAndA.SortingAnyTypeOfObject
+{
+    public class Circle
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public static bool operator >(Circle circle1, Circle circle2)
+        {
+            return circle1.Radius > circle2.Radius;
+        }
+
+        public static bool operator <(Circle circle1, Circle circle2)
+        {
+            return circle1.Radius < circle2.Radius;
+        }
+    }
+}
