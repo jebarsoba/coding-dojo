59c0c201dec5f4d33922cccee1b9e76274da06d9
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-09-03 12:20:48 -0300

    Working on the char duplicates check algorithm.

---
+++ udemy/ds-and-a/DSAndA.Test/DuplicatesChecker.Test.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace DSAndA.Test
+{
+    public class DuplicatesCheckerTestFixture
+    {
+        [Fact]
+        public void DuplicatesCheckerTest1()
+        {
+            Assert.True(new DuplicatesChecker().HasDuplicateCharacters("anaconda"));
+        }
+
+        [Fact]
+        public void DuplicatesCheckerTest2()
+        {
+            Assert.False(new DuplicatesChecker().HasDuplicateCharacters("great"));
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/DuplicatesChecker.cs
@@ -0,0 +1,22 @@
+﻿namespace DSAndA
+{
+    public class DuplicatesChecker
+    {
+        /// <summary>
+        /// The time complexity of this algorithm is O(N square).
+        /// </summary>
+        /// <param name="literal"></param>
+        /// <returns></returns>
+        public bool HasDuplicateCharacters(string literal)
+        {
+            char[] characters = literal.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+                for (int j = i + 1; j < characters.Length; j++)
+                    if (characters[j] == characters[i])
+                        return true;
+
+            return false;
+        }
+    }
+}
