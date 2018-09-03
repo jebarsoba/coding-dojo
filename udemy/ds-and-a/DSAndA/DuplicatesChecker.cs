602e154d0cc24f5f4589db98f6d83898cf4d3384
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-09-03 14:29:06 -0300

    Refactoring char duplicates check algorithm.

--- udemy/ds-and-a/DSAndA/DuplicatesChecker.cs
+++ udemy/ds-and-a/DSAndA/DuplicatesChecker.cs
@@ -1,20 +1,25 @@
-﻿namespace DSAndA
+﻿using System.Collections.Generic;
+
+namespace DSAndA
 {
     public class DuplicatesChecker
     {
         /// <summary>
-        /// The time complexity of this algorithm is O(N square).
+        /// The time complexity of this algorithm is O(N).
         /// </summary>
         /// <param name="literal"></param>
         /// <returns></returns>
         public bool HasDuplicateCharacters(string literal)
         {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
             char[] characters = literal.ToCharArray();
 
             for (int i = 0; i < characters.Length; i++)
-                for (int j = i + 1; j < characters.Length; j++)
-                    if (characters[j] == characters[i])
-                        return true;
+                if (dict.ContainsKey(characters[i]))
+                    return true;
+                else
+                    dict[characters[i]] = i;
 
             return false;
         }
