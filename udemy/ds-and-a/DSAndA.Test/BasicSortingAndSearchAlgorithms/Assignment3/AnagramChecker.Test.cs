93a98b61d1202b781b5cd9c5c65cd2a119ea1835
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-09-13 16:06:17 -0300

    Anagram checker algorithm implemented (Section 3 - Lecture 28 - Assignment 3).

---
+++ udemy/ds-and-a/DSAndA.Test/BasicSortingAndSearchAlgorithms/Assignment3/AnagramChecker.Test.cs
@@ -0,0 +1,14 @@
+﻿using DSAndA.BasicSortingAndSearchAlgorithms.Assignment3;
+using Xunit;
+
+namespace DSAndA.Test.BasicSortingAndSearchAlgorithms.Assignment3
+{
+    public class AnagramCheckerTestFixture
+    {
+        [Fact]
+        public void AnagramCheckerTest1()
+        {
+            Assert.True(new AnagramChecker().IsAnagram("letras", "lastre"));
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/BasicSortingAndSearchAlgorithms/Assignment3/AnagramChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSAndA.BasicSortingAndSearchAlgorithms.Assignment3
+{
+    public class AnagramChecker
+    {
+        public bool IsAnagram(string wordOne, string wordTwo)
+        {
+            char[] wordOneLetters = wordOne.ToCharArray();
+            char[] wordTwoLetters = wordTwo.ToCharArray();
+
+            if (wordOneLetters.Length != wordTwoLetters.Length)
+                throw new ApplicationException("Words have different lenghts and cannot be anagrams.");
+
+            Dictionary<char, int> wordOneLettersDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < wordOneLetters.Length; i++)
+                wordOneLettersDict[wordOneLetters[i]] = i;
+
+            for (int i = 0; i < wordTwoLetters.Length; i++)
+                if (!wordOneLettersDict.ContainsKey(wordTwoLetters[0]))
+                    return false;
+
+            return true;
+        }
+    }
+}
