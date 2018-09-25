d37062508fe8354df60769ab674d4a27cc1eb93a
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-09-25 16:25:52 -0300

    Insertion at the start and end of a doubly ended list algorithms done.

---
+++ udemy/ds-and-a/DSAndA.Test/LinkedLists/DoublyEndedList.Test.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace DSAndA.Test.LinkedLists
+{
+    public class DoublyEndedListTestFixture
+    {
+        [Fact]
+        public void InsertAtHeadTest1()
+        {
+            DoublyEndedList doublyEndedList = new DoublyEndedList();
+            doublyEndedList.InsertAtHead(1);
+
+            Assert.Equal(expected: "1,", actual: doublyEndedList.ToString());
+        }
+
+        [Fact]
+        public void InsertAtTailTest1()
+        {
+            DoublyEndedList doublyEndedList = new DoublyEndedList();
+            doublyEndedList.InsertAtHead(1);
+            doublyEndedList.InsertAtHead(5);
+            Assert.Equal(expected: "5,1,", actual: doublyEndedList.ToString());
+
+            doublyEndedList.InsertAtTail(6);
+            Assert.Equal(expected: "5,1,6,", actual: doublyEndedList.ToString());
+        }
+
+        [Fact]
+        public void InsertAtTailTest2()
+        {
+            DoublyEndedList doublyEndedList = new DoublyEndedList();
+            doublyEndedList.InsertAtTail(6);
+
+            Assert.Equal(expected: "6,", actual: doublyEndedList.ToString());
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/DoublyEndedList.cs
@@ -0,0 +1,53 @@
+﻿using DSAndA.LinkedLists;
+
+namespace DSAndA
+{
+    public class DoublyEndedList
+    {
+        private Node<int> head;
+        private Node<int> tail;
+
+        public void InsertAtHead(int data)
+        {
+            Node<int> newNode = new Node<int>(data);
+
+            newNode.NextNode = this.head;
+            this.head = newNode;
+
+            if (this.tail == null)
+                this.tail = newNode;
+        }
+
+        public void InsertAtTail(int data)
+        {
+            Node<int> newNode = new Node<int>(data);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+
+                return;
+            }
+
+            this.tail.NextNode = newNode;
+            this.tail = newNode;
+        }
+
+        public override string ToString()
+        {
+            string nodes = "";
+
+            Node<int> current = this.head;
+
+            while (current != null)
+            {
+                nodes += $"{current.ToString()},";
+
+                current = current.NextNode;
+            }
+
+            return nodes;
+        }
+    }
+}
