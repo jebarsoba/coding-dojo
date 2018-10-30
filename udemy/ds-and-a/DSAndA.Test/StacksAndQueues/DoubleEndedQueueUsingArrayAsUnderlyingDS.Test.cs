0f53e2171f13c16478839adb999c6a2d23f9688b
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-10-30 11:36:56 -0300

    Double ended queue using an array as the underlying data structure implemented.

---
+++ udemy/ds-and-a/DSAndA.Test/StacksAndQueues/DoubleEndedQueueUsingArrayAsUnderlyingDS.Test.cs
@@ -0,0 +1,41 @@
+﻿using DSAndA.StacksAndQueues;
+using Xunit;
+
+namespace DSAndA.Test.StacksAndQueues
+{
+    public class DoubleEndedQueueUsingArrayAsUnderlyingDSTestFixture
+    {
+        [Fact]
+        public void DoubleEndedQueueUsingArrayAsUnderlyingDSTest1()
+        {
+            DoubleEndedQueueUsingArrayAsUnderlyingDS arrayDEQueue = new DoubleEndedQueueUsingArrayAsUnderlyingDS(maxsize: 8);
+            arrayDEQueue.InsertRight(7);
+
+            Assert.Equal(expected: 7, actual: arrayDEQueue.DeleteLeft());
+        }
+
+        [Fact]
+        public void DoubleEndedQueueUsingArrayAsUnderlyingDSTest2()
+        {
+            DoubleEndedQueueUsingArrayAsUnderlyingDS arrayDEQueue = new DoubleEndedQueueUsingArrayAsUnderlyingDS(maxsize: 8);
+            arrayDEQueue.InsertRight(7);
+            arrayDEQueue.InsertRight(12);
+
+            arrayDEQueue.InsertLeft(14);
+
+            Assert.Equal(expected: 14, actual: arrayDEQueue.DeleteLeft());
+        }
+
+        [Fact]
+        public void DoubleEndedQueueUsingArrayAsUnderlyingDSTest3()
+        {
+            DoubleEndedQueueUsingArrayAsUnderlyingDS arrayDEQueue = new DoubleEndedQueueUsingArrayAsUnderlyingDS(maxsize: 8);
+            arrayDEQueue.InsertRight(7);
+            arrayDEQueue.InsertRight(12);
+
+            arrayDEQueue.InsertLeft(14);
+
+            Assert.Equal(expected: 12, actual: arrayDEQueue.DeleteRight());
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/StacksAndQueues/DoubleEndedQueueUsingArrayAsUnderlyingDS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DSAndA.StacksAndQueues
+{
+    public class DoubleEndedQueueUsingArrayAsUnderlyingDS
+    {
+        private int maxsize;
+        private int[] doubleEndedQueueArray;
+        private int head;
+        private int tail;
+
+        private bool IsEmpty => this.head == -1;
+        private bool HasFreeSpacesAtLeft => this.head > 0;
+        private bool HasFreeSpacesAtRight => this.tail < this.maxsize - 1;
+
+        public DoubleEndedQueueUsingArrayAsUnderlyingDS(int maxsize)
+        {
+            this.maxsize = maxsize;
+            this.doubleEndedQueueArray = new int[this.maxsize];
+            this.head = -1;
+            this.tail = -1;
+        }
+
+        public void InsertRight(int element)
+        {
+            if (this.IsEmpty) ++this.head;
+
+            this.doubleEndedQueueArray[++this.tail] = element;
+        }
+
+        public int DeleteLeft()
+        {
+            return this.doubleEndedQueueArray[this.head++];
+        }
+
+        public void InsertLeft(int element)
+        {
+            if (!this.HasFreeSpacesAtLeft && !this.HasFreeSpacesAtRight)
+                throw new ApplicationException("Double ended queue is full.");
+
+            if (!this.HasFreeSpacesAtLeft && this.HasFreeSpacesAtRight)
+                this.head = this.maxsize;
+
+            this.doubleEndedQueueArray[--this.head] = element;
+        }
+
+        public int DeleteRight()
+        {
+            return this.doubleEndedQueueArray[this.tail--];
+        }
+    }
+}
