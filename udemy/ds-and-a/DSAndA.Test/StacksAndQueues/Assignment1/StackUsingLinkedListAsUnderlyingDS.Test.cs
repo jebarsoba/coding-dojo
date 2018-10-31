718fed9bfa6bdc97eed1d9c37e97039c5e2c8e04
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-10-31 13:14:56 -0300

    Stack using a linked list as the underlying data structure implemented (Section 5 - Lecture 48 - Assignment 1).

---
+++ udemy/ds-and-a/DSAndA.Test/StacksAndQueues/Assignment1/StackUsingLinkedListAsUnderlyingDS.Test.cs
@@ -0,0 +1,41 @@
+﻿using DSAndA.StacksAndQueues;
+using Xunit;
+
+namespace DSAndA.Test.StacksAndQueues.Assignment1
+{
+    public class StackUsingLinkedListAsUnderlyingDSTestFixture
+    {
+        [Fact]
+        public void StackUsingLinkedListAsUnderlyingDSTest1()
+        {
+            StackUsingLinkedListAsUnderlyingDS linkedListStack = new StackUsingLinkedListAsUnderlyingDS();
+            linkedListStack.Push(5);
+
+            Assert.Equal(expected: 5, actual: linkedListStack.Peek());
+        }
+
+        [Fact]
+        public void StackUsingLinkedListAsUnderlyingDSTest2()
+        {
+            StackUsingLinkedListAsUnderlyingDS linkedListStack = new StackUsingLinkedListAsUnderlyingDS();
+            linkedListStack.Push(5);
+            linkedListStack.Push(12);
+
+            Assert.Equal(expected: 12, actual: linkedListStack.Peek());
+        }
+
+        [Fact]
+        public void StackUsingLinkedListAsUnderlyingDSTest3()
+        {
+            StackUsingLinkedListAsUnderlyingDS linkedListStack = new StackUsingLinkedListAsUnderlyingDS();
+            linkedListStack.Push(5);
+            linkedListStack.Push(12);
+            linkedListStack.Push(7);
+            linkedListStack.Push(2);
+
+            Assert.Equal(expected: 2, actual: linkedListStack.Pop());
+            Assert.Equal(expected: 7, actual: linkedListStack.Pop());
+            Assert.Equal(expected: 12, actual: linkedListStack.Peek());
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/StacksAndQueues/StackUsingLinkedListAsUnderlyingDS.cs
@@ -0,0 +1,33 @@
+﻿using DSAndA.LinkedLists;
+
+namespace DSAndA.StacksAndQueues
+{
+    public class StackUsingLinkedListAsUnderlyingDS
+    {
+        private DoublyLinkedNode<int> top;
+
+        public void Push(int element)
+        {
+            DoublyLinkedNode<int> newNode = new DoublyLinkedNode<int>(element);
+            newNode.Prev = this.top;
+
+            if (this.top != null) this.top.Next = newNode;
+
+            this.top = newNode;
+        }
+
+        public int Peek()
+        {
+            return this.top.Data;
+        }
+
+        public int Pop()
+        {
+            DoublyLinkedNode<int> poppedNode = this.top;
+
+            this.top = this.top.Prev;
+
+            return poppedNode.Data;
+        }
+    }
+}
