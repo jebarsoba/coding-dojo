7eb5aca814ca2928124402a9453ffb05bb70e651
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-11-02 10:52:23 -0300

    Queue using a linked list as the underlying data structure implemented (Section 5 - Lecture 48 - Assignment 3).

---
+++ udemy/ds-and-a/DSAndA.Test/StacksAndQueues/Assignment3/QueueUsingLinkedListAsUnderlyingDS.Test.cs
@@ -0,0 +1,27 @@
+﻿using DSAndA.StacksAndQueues.Assignment3;
+using Xunit;
+
+namespace DSAndA.Test.StacksAndQueues.Assignment3
+{
+    public class QueueUsingLinkedListAsUnderlyingDSTestFixture
+    {
+        [Fact]
+        public void QueueUsingLinkedListAsUnderlyingDSTest1()
+        {
+            QueueUsingLinkedListAsUnderlyingDS linkedListQueue = new QueueUsingLinkedListAsUnderlyingDS();
+            linkedListQueue.Push(1);
+
+            Assert.Equal(expected: 1, actual: linkedListQueue.Peek());
+        }
+
+        [Fact]
+        public void QueueUsingLinkedListAsUnderlyingDSTest2()
+        {
+            QueueUsingLinkedListAsUnderlyingDS linkedListQueue = new QueueUsingLinkedListAsUnderlyingDS();
+            linkedListQueue.Push(1);
+            linkedListQueue.Push(2);
+
+            Assert.Equal(expected: 1, actual: linkedListQueue.Pop());
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/StacksAndQueues/Assignment3/QueueUsingLinkedListAsUnderlyingDS.cs
@@ -0,0 +1,39 @@
+﻿using DSAndA.LinkedLists;
+
+namespace DSAndA.StacksAndQueues.Assignment3
+{
+    public class QueueUsingLinkedListAsUnderlyingDS
+    {
+        private Node<int> head;
+        private Node<int> tail;
+
+        private bool IsEmpty => this.tail == null;
+
+        public void Push(int element)
+        {
+            Node<int> newNode = new Node<int>(element);
+
+            if (!this.IsEmpty)
+                this.tail.NextNode = newNode;
+            else
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+        }
+
+        public int Pop()
+        {
+            int poppedElement = this.head.GetData();
+
+            this.head = this.head.NextNode;
+
+            return poppedElement;
+        }
+
+        public int Peek()
+        {
+            return this.head.GetData();
+        }
+    }
+}
