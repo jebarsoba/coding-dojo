4bea36c2cc37527aaa2366d8f68ed0d3777a1c08
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-11-05 12:21:23 -0300

    Working on an algorithm to evaluate a math expression in prefix form (Section 5 - Lecture 48 - Assignment 4).

---
+++ udemy/ds-and-a/DSAndA.Test/StacksAndQueues/Assignment4/MathPrefixExpressionEvaluator.Test.cs
@@ -0,0 +1,26 @@
+﻿using DSAndA.StacksAndQueues;
+using Xunit;
+
+namespace DSAndA.Test.StacksAndQueues.Assignment4
+{
+    public class MathPrefixExpressionEvaluatorTestFixture
+    {
+        [Fact]
+        public void MathPrefixExpressionEvaluatorTest1()
+        {
+            Assert.Equal(expected: 6.0, actual: new MathPrefixExpressionEvaluator().Evaluate("+ 3 3"));
+        }
+
+        [Fact]
+        public void MathPrefixExpressionEvaluatorTest2()
+        {
+            Assert.Equal(expected: 9.0, actual: new MathPrefixExpressionEvaluator().Evaluate("+ 3 ( + 3 3 )"));
+        }
+
+        /*[Fact]
+        public void MathPrefixExpressionEvaluatorTest3()
+        {
+            Assert.Equal(expected: 12.0, actual: new MathPrefixExpressionEvaluator().Evaluate("+ 3 ( + ( + 3 3 ) 3 )"));
+        }*/
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/StacksAndQueues/Assignment4/MathPrefixExpressionEvaluator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DSAndA.StacksAndQueues
+{
+    public class MathPrefixExpressionEvaluator
+    {
+        public double Evaluate(string prefixExpression)
+        {
+            string[] expressionArray = prefixExpression.Split(new char[] { ' ' });
+
+            return this.ApplyOperator(expressionArray[0], this.Evaluate(expressionArray, 1), this.Evaluate(expressionArray, 2));
+        }
+
+        private double Evaluate(string[] expressionArray, int index)
+        {
+            if (expressionArray[index] == "(")
+            {
+                string subExpression = "";
+
+                while (expressionArray[++index] != ")")
+                    subExpression += $"{expressionArray[index]} ";
+
+                return this.Evaluate(subExpression.Trim());
+            }
+
+            return int.Parse(expressionArray[index]);
+        }
+
+        private double ApplyOperator(string oper8or, double firstOperand, double secondOperand)
+        {
+            if (oper8or == "+")
+                return firstOperand + secondOperand;
+
+            throw new NotImplementedException($"{oper8or} operator not yet implemented.");
+        }
+    }
+}
