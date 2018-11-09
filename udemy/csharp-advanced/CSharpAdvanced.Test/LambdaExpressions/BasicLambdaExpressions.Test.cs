dba8ebf5edfdd23df73b59d45e852da789acbd81
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-11-09 08:09:17 -0300

    Practicing C# Lambda expressions.

---
+++ udemy/csharp-advanced/CSharpAdvanced.Test/LambdaExpressions/BasicLambdaExpressions.Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace CSharpAdvanced.Test.LambdaExpressions
+{
+    public class BasicLambdaExpressionsTestFixture
+    {
+        [Fact]
+        public void SquareTest()
+        {
+            Func<int, int> square = number => number * number;
+
+            Assert.Equal(expected: 25, actual: square(5));
+        }
+
+        [Fact]
+        public void MultiplierTest()
+        {
+            const int factor = 5;
+
+            Func<int, int> multiplier = number => number * factor;
+
+            Assert.Equal(expected: 50, actual: multiplier(10));
+        }
+    }
+}
