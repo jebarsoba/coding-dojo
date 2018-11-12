8450c1c327cc1f494e67e48ecdcd8ca429c0e781
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-11-12 16:17:59 -0300

    C# Lambda expressions course section done.

---
+++ udemy/csharp-advanced/CSharpAdvanced.Test/LambdaExpressions/BookRepository.Test.cs
@@ -0,0 +1,20 @@
+﻿using CSharpAdvanced.Generics;
+using CSharpAdvanced.LambdaExpressions;
+using Xunit;
+
+namespace CSharpAdvanced.Test.LambdaExpressions
+{
+    public class BookRepositoryTestFixture
+    {
+        [Fact]
+        public void BookRepositoryTest1()
+        {
+            BookRepository repo = new BookRepository();
+            repo.Save(new Book(isbn: "1111", title: "C# Advanced Topics") { Price = 1 });
+            repo.Save(new Book(isbn: "2222", title: "Introduction to Data Structures & Algorithms in Java") { Price = 2 });
+
+            Assert.Equal(expected: 1, actual: repo.Find(book => book.Price < 2).Count);
+            Assert.Equal(expected: "1111", actual: repo.Find(book => book.Price < 2)[0].Isbn);
+        }
+    }
+}
---
+++ udemy/csharp-advanced/CSharpAdvanced/LambdaExpressions/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using CSharpAdvanced.Generics;
+using System;
+using System.Collections.Generic;
+
+namespace CSharpAdvanced.LambdaExpressions
+{
+    public class BookRepository
+    {
+        private List<Book> books = new List<Book>();
+
+        public void Save(Book book)
+        {
+            this.books.Add(book);
+        }
+
+        public IList<Book> Find(Predicate<Book> predicate)
+        {
+            return this.books.FindAll(predicate);
+        }
+    }
+}
