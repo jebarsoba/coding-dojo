61fa3feb7e3d33b640e2ce382766c7ff8d2e3aa5
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-11-14 15:08:51 -0300

    Factorial algorithm implemented, two versions, using head and tail recursion.

---
+++ udemy/ds-and-a/DSAndA.Test/Recursion/Factorial.Test.cs
@@ -0,0 +1,32 @@
+﻿using DSAndA.Recursion;
+using Xunit;
+
+namespace DSAndA.Test.Recursion
+{
+    public class FactorialTestFixture
+    {
+        [Fact]
+        public void FactorialTest1()
+        {
+            Assert.Equal(expected: 6, actual: new Factorial().Calculate(3));
+        }
+
+        [Fact]
+        public void FactorialTest2()
+        {
+            Assert.Equal(expected: 1, actual: new Factorial().CalculateImplementingTailRecursion(1));
+        }
+
+        [Fact]
+        public void FactorialTest3()
+        {
+            Assert.Equal(expected: 2, actual: new Factorial().CalculateImplementingTailRecursion(2));
+        }
+
+        [Fact]
+        public void FactorialTest4()
+        {
+            Assert.Equal(expected: 6, actual: new Factorial().CalculateImplementingTailRecursion(3));
+        }
+    }
+}
---
+++ udemy/ds-and-a/DSAndA/Recursion/Factorial.cs
@@ -0,0 +1,20 @@
+﻿namespace DSAndA.Recursion
+{
+    public class Factorial
+    {
+        public int Calculate(int number)
+        {
+            return number * (number > 1 ? this.Calculate(number - 1) : 1);
+        }
+
+        public int CalculateImplementingTailRecursion(int number)
+        {
+            return this.CalculateImplementingTailRecursion(number: number, result: 1);
+        }
+
+        private int CalculateImplementingTailRecursion(int number, int result)
+        {
+            return number != 0 ? this.CalculateImplementingTailRecursion(number: number - 1, result: result * number) : result;
+        }
+    }
+}
