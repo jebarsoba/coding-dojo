6fe06173708a930d0a40532c8c93d6c4a8c5f722
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-12-04 10:56:23 -0300

    Practicing different approaches to implement private attributes: symbols.

--- others/javascript/src/tests/closure-vs-symbol.test.js
+++ others/javascript/src/tests/closure-vs-symbol.test.js
@@ -1,15 +1,42 @@
-var Pet = (() => {
-  function Pet(type) {
-    this.getType = () => {
-      return type; // getTYpe will keep having access to type, even after the object creation itself...
-    };
-  }
-
-  return Pet;
-})();
-
 test('Implementing private attributes using closures.', done => {
-  var a = new Pet('dog');
+  let Pet = (() => {
+    function Pet(type) {
+      this.getType = () => {
+        return type; // getTYpe will keep having access to type, even after the object creation itself...
+      };
+    }
+
+    return Pet;
+  })();
+
+  let a = new Pet('dog');
+
+  expect(a.getType()).toBe('dog');
+
+  //Changing from outside (attempt)...
+  a.type = null;
+
+  expect(a.getType()).toBe('dog');
+
+  done();
+});
+
+test('Implementing private attributes using the Symbol ES6 primitive (although they\'ll be available through getOwnPropertySymbols).', done => {
+  let Pet = (() => {
+    let typeSymbol = Symbol('type');
+
+    function Pet(type) {
+      this[typeSymbol] = type;
+    }
+
+    Pet.prototype.getType = function() {
+      return this[typeSymbol];
+    };
+
+    return Pet;
+  })();
+
+  let a = new Pet('dog');
 
   expect(a.getType()).toBe('dog');
 
