a5d366763693d687b48b586257fdf96315150a7e
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-12-07 16:23:11 -0300

    Practicing delete.

--- others/javascript/src/tests/miscellaneous.js
+++ others/javascript/src/tests/miscellaneous.js
@@ -15,9 +15,9 @@ test('setTimeout and the event loop.', done => {
 });
 
 test('Delete and variables.', done => {
-  var x = 1;
+  let x = 1;
 
-  var output = (function () {
+  let output = (function () {
     // ES5 marks this line as syntax error, so it's not possible to run this question as is...
     // delete x; // Deletes both an object property and its value, but no effect on variables...
 
@@ -25,6 +25,34 @@ test('Delete and variables.', done => {
   })();
 
   expect(output).toBe(1);
+
+  done();
+});
+
+test('Delete and object properties', done => {
+  let x = { foo: 1 };
+
+  let output = (function () {
+    delete x.foo;
+
+    return x.foo;
+  }();
+
+  expect(output).toBe(undefined);
+
+  done();
+});
+
+test('Delete and Object.create properties.', done => {
+  let Employee = {
+    company: 'xyz'
+  };
+
+  let empl = Object.create(Employee);
+
+  delete empl.company; // By default, properties of an object created through Object.create are not writable. That's why delete will have no effect here...
+
+  expect(empl.company).toBe('xyz')
 
   done();
 });
