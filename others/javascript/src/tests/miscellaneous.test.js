8aa4f545b5c72f5bb3f13a21afb9e3251b354b52
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-01-02 07:53:59 -0300

    JS practice: Functional programming + Spread operator.

--- others/javascript/src/tests/miscellaneous.test.js
+++ others/javascript/src/tests/miscellaneous.test.js
@@ -129,3 +129,34 @@ test('Use of the this keyword practice.', done => {
 
   view.getButton().firePress();
 });
+
+  test('Functional programming + Spread operator.', done => {
+  const spotifySongs = [
+    { id: 1, name: 'Curl of the Burl', artist: 'Mastodon', duration: 204 },
+    { id: 2, name: 'Oblivion', artist: 'Mastodon', duration: 306 },
+    { id: 3, name: 'Flying Whales', artist: 'Gojire', duration: 444 },
+    { id: 4, name: 'LEnfant Sauvage', artist: 'Gojira', duration: 246}
+  ];
+
+  const lastFmSongs = [
+    { id: 5, name: 'Chop Suey', artist: 'System of a Down', duration: 192 },
+    { id: 6, name: 'Throne', artist: 'Bring me the Horizon', duration: 186 },
+    { id: 7, name: 'Destrier', artist: 'Agent Fresco', duration: 132 },
+    { id: 8, name: 'Out of the Black', artist: 'Royal Blood', duration: 240 }
+  ];
+
+  const allSongs = [spotifySongs, lastFmSongs];
+
+  const songNames = allSongs.reduce((acc, currValue) => {
+      return acc.concat(currValue);
+    }, [])
+    .map(song => {
+      return {...song, duration: Math.floor(song.duration / 60)};
+    })
+    .filter(song => song.duration > 3)
+    .map(song => song.name).join(" , ");
+
+  expect(songNames).toBe("Oblivion , Flying Whales , LEnfant Sauvage , Out of the Black");
+
+  done();
+});
