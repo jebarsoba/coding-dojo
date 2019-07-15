7305dfcd5c5518d2317a1734116c41682cb116d6
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-15 11:12:42 -0300

    OpenUI5 quick start, step 2.

---
+++ others/openui5/quick-start/webapp/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("Quickstart.App", {
+		onPress : function () {
+			MessageToast.show("Hello App!");
+		}
+	});
+});
--- others/openui5/quick-start/webapp/index.js
+++ others/openui5/quick-start/webapp/index.js
@@ -1,14 +1,9 @@
 sap.ui.define([
-	"sap/m/Button",
-	"sap/m/MessageToast"
-], function (Button, MessageToast) {
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
-	new Button({
-		text: "Ready...",
-		press: function () {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
-
+	XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+		oView.placeAt("content");
+	});
 });
