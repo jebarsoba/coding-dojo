f387a4f794c87f26d218577a5e5b502908f96bd2
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-16 12:41:22 -0300

    OpenUI5 walkthrough, step 3.

--- others/openui5/walkthrough/webapp/index.js
+++ others/openui5/walkthrough/webapp/index.js
@@ -1,5 +1,9 @@
-sap.ui.define([], function () {
+sap.ui.define([
+    "sap/m/Text"
+], function (Text) {
     "use strict";
-    
-    alert("UI5 is ready");
+
+    new Text({
+        text: "Hello World"
+	}).placeAt("content");
 });
