ee8089a21bacb9a10e04e0ef5322738c68ae0669
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-17 09:51:28 -0300

    OpenUI5 walkthrough, step 5.

---
+++ others/openui5/walkthrough/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+ ], function (Controller) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onShowHello: function () {
+           // show a native JavaScript alert
+           alert("Hello World");
+        }
+     });
+ });
