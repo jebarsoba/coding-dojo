23fcb7fb1b9bec58d442cd365d0ac53e6a48efee
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-18 15:09:27 -0300

    OpenUI5 walkthrough, step 6.

--- others/openui5/walkthrough/webapp/controller/App.controller.js
+++ others/openui5/walkthrough/webapp/controller/App.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
         onShowHello: function () {
-           // show a native JavaScript alert
-           alert("Hello World");
+           MessageToast.show("Hello World");
         }
      });
  });
