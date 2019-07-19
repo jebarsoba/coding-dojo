a1b6ab4ab610ad3791dd2c9837fa7edcc43e1ea5
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-19 17:52:39 -0300

    OpenUI5 walkthrough, step 15.

--- others/openui5/walkthrough/webapp/controller/App.controller.js
+++ others/openui5/walkthrough/webapp/controller/App.controller.js
@@ -1,16 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
  ], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello: function () {
-         // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-         // show message
-         MessageToast.show(sMsg);
-        }
      });
  });
