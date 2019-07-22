4f50677bf4a4a28e18fe7c028d778a8ef8e5655c
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-07-22 09:29:33 -0300

    OpenUI5 walkthrough, step 16.

--- others/openui5/walkthrough/webapp/controller/HelloPanel.controller.js
+++ others/openui5/walkthrough/webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+ ], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
        onShowHello: function () {
@@ -11,6 +12,24 @@ sap.ui.define([
           var sMsg = oBundle.getText("helloMsg", [sRecipient]);
           // show message
           MessageToast.show(sMsg);
+       },
+       onOpenDialog: async function () {
+           var oView = this.getView();
+
+           // create dialog lazily
+           if (!this.byId("helloDialog")) {
+                // load asynchronous XML fragment
+                let oDialog = await Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                });
+                
+                // connect dialog to the root view of this component (models, lifecycle)
+                oView.addDependent(oDialog);
+                oDialog.open();
+           } else {
+               this.byId("helloDialog").open();
+           }
        }
     });
  });
