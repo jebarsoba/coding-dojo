efa9ea46751c8ff2aed81cd27a3f063121419301
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2021-02-04 07:42:15 -0300

    stack using linked list as underlying ds

---
+++ udemy/ds-and-a/javascript-solutions/src/StackAndQueues/Assignment1/StackUsingLinkedListAsUnderlyingDS.js
@@ -0,0 +1,49 @@
+﻿const DoublyLinkedNode = require("../../LinkedLists/DoublyLinkedNode");
+
+class StackUsingLinkedListAsUnderlyingDS {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(data) {
+        let newNode = new DoublyLinkedNode(data);
+        newNode.prevNode = this.tail;
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            this.tail.nextNode = newNode;
+        }
+
+        this.tail = newNode;
+    }
+
+    pop() {
+        const itemToPop = this.tail;
+
+        this.tail.prevNode.nextNode = null;
+        this.tail = this.tail.prevNode;
+
+        return itemToPop.data;
+    }
+
+    peek() {
+        return this.tail.data;
+    }
+
+    toString() {
+        let result = [];
+        let current = this.head;
+
+        while (current != null) {
+            result.push(current.data);
+
+            current = current.nextNode;
+        }
+
+        return result.join(', ');
+    }
+}
+
+module.exports = StackUsingLinkedListAsUnderlyingDS;
---
+++ udemy/ds-and-a/javascript-solutions/tests/StacksAndQueues/Assignment1/StackUsingLinkedListAsUnderlyingDS.test.js
@@ -0,0 +1,38 @@
+﻿const StackUsingLinkedListAsUnderlyingDS = require('../../../src/StackAndQueues/Assignment1/StackUsingLinkedListAsUnderlyingDS');
+
+test('should push item into stack', () => {
+    let stack = new StackUsingLinkedListAsUnderlyingDS();
+
+    stack.push(5);
+    stack.push(15);
+    stack.push(30);
+
+    expect(stack.toString()).toBe('5, 15, 30');
+});
+
+test('should pop item from stack', () => {
+    let stack = new StackUsingLinkedListAsUnderlyingDS();
+
+    stack.push(5);
+    stack.push(15);
+    stack.push(30);
+
+    expect(stack.toString()).toBe('5, 15, 30');
+
+    const item = stack.pop();
+
+    expect(item).toBe(30);
+    expect(stack.toString()).toBe('5, 15');
+});
+
+test('should peek item in stack', () => {
+    let stack = new StackUsingLinkedListAsUnderlyingDS();
+
+    stack.push(5);
+    stack.push(15);
+    stack.push(30);
+
+    expect(stack.toString()).toBe('5, 15, 30');
+
+    expect(stack.peek()).toBe(30);
+});
