dc43d0aa31acdc14db4abc5f815add2dc7d9cee6
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2021-02-13 15:56:29 -0300

    tower of hanoi time complexity

--- udemy/ds-and-a/javascript-solutions/src/Recursion/TowerOfHanoi.js
+++ udemy/ds-and-a/javascript-solutions/src/Recursion/TowerOfHanoi.js
@@ -8,6 +8,7 @@ class TowerOfHanoi {
             'C': new Stack()
         };
         this.length = 0;
+        this.moves = 0;
     }
 
     push(disc) {
@@ -19,7 +20,7 @@ class TowerOfHanoi {
         if (numberOfDiscs === 1) {
             this.popFromOneAndPushIntoAnother(from, to);
 
-            return this.toString();
+            return;
         }
 
         this.move(numberOfDiscs - 1, from, intermediate, to);
@@ -30,9 +31,17 @@ class TowerOfHanoi {
     popFromOneAndPushIntoAnother(from, to) {
         // console.log(`Moving disc from ${from} to ${to}`);
         this.towers[to].push(this.towers[from].pop());
+        this.moves++;
     }
 
     toString() {
+        // With 2 discs, 3 moves are needed...
+        // With 3 discs, 7 moves are needed...
+        // With 4 discs, 15 moves ares needed...
+        // With 5 discs, 31 moves ares needed...
+        // So, time complexity is O(2^n - 1)
+        console.log(`Number of moves: ${this.moves}`);
+
         return `A ${this.towers['A'].toString()}; B ${this.towers['B'].toString()}; C ${this.towers['C'].toString()}`
     }
 }
--- udemy/ds-and-a/javascript-solutions/tests/Recursion/TowerOfHanoi.test.js
+++ udemy/ds-and-a/javascript-solutions/tests/Recursion/TowerOfHanoi.test.js
@@ -28,3 +28,36 @@ test('Solving tower with 3 discs', () => {
 
   expect(towerOfHanoi.toString()).toBe('A ; B ; C 3, 2, 1');
 });
+
+test('Solving tower with 4 discs', () => {
+  const towerOfHanoi = new TowerOfHanoi();
+  towerOfHanoi.push(4);
+  towerOfHanoi.push(3);
+  towerOfHanoi.push(2);
+  towerOfHanoi.push(1);
+
+  const numberOfDiscs = 4;
+  const from = 'A';
+  const to = 'C';
+  const intermediate = 'B';
+  towerOfHanoi.move(numberOfDiscs, from, to, intermediate);
+
+  expect(towerOfHanoi.toString()).toBe('A ; B ; C 4, 3, 2, 1');
+});
+
+test('Solving tower with 5 discs', () => {
+  const towerOfHanoi = new TowerOfHanoi();
+  towerOfHanoi.push(5);
+  towerOfHanoi.push(4);
+  towerOfHanoi.push(3);
+  towerOfHanoi.push(2);
+  towerOfHanoi.push(1);
+
+  const numberOfDiscs = 5;
+  const from = 'A';
+  const to = 'C';
+  const intermediate = 'B';
+  towerOfHanoi.move(numberOfDiscs, from, to, intermediate);
+
+  expect(towerOfHanoi.toString()).toBe('A ; B ; C 5, 4, 3, 2, 1');
+});
