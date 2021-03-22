cfa2766f08e839cb036051f868e97fcb65cc41e5
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2021-03-22 09:01:33 -0300

    leetcode 21, merge two sorted lists

---
+++ leetcode/javascript-solutions/src/21-merge-two-sorted-lists.js
@@ -0,0 +1,68 @@
+// Definition for singly-linked list
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+const mergeTwoLists = (l1, l2) => {
+    const insertSorted = (val, list) => {
+        if (val === undefined) {
+            return {};
+        }
+
+        if (!list || list.val === undefined) {
+            list = {};
+            ListNode.call(list, val);
+            return list;
+        }
+
+        let inserted = false;
+        let prev = null;
+        let cur = list;
+        while (cur) {
+            if (cur.val > val) {
+                let newNode = {};
+                ListNode.call(newNode, val, cur);
+
+                if (prev) {
+                    prev.next = newNode;
+                } else {
+                    list = newNode;
+                }
+
+                inserted = true;
+                return list;
+            }
+
+            prev = cur;
+            cur = cur.next;
+        }
+
+        if (!inserted) {
+            let newNode = {};
+            ListNode.call(newNode, val);
+            prev.next = newNode;
+        }
+
+        return list;
+    };
+
+    let res = null;
+
+    // The whole solution could be simplified by using a single while loop to traverse both lists at the same time ...
+    let cur = l1;
+    while (cur) {
+        res = insertSorted(cur.val, res);
+        cur = cur.next;
+    }
+
+    cur = l2;
+    while (cur) {
+        res = insertSorted(cur.val, res);
+        cur = cur.next;
+    }
+
+    return res;
+};
+
+module.exports = { ListNode, mergeTwoLists };
