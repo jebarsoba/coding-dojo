98958cc72b3842379e6029026e8b0d7e6ba8c71e
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2021-03-26 13:15:57 -0300

    leetcode 53, maximum subarray

---
+++ leetcode/javascript-solutions/src/53-maximum-subarray_brute-force.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let max;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i; j < nums.length; j++) {
+            const sum = nums.slice(i, j + 1).reduce((acc, num) => { acc += num; return acc; }, 0);
+            if (max === undefined || sum > max) max = sum;
+        }
+    }
+
+    return max;
+};
+
+module.exports = maxSubArray;
---
+++ leetcode/javascript-solutions/src/53-maximum-subarray_kadane.js
@@ -0,0 +1,16 @@
+/**
+ * This solution is based on the Kadane's algorithm. Here's a good explanation about it:
+ * https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let localMax;
+    return nums.reduce((globalMax, currValue, currIndex) => {
+        localMax = currIndex > 0 ? Math.max(currValue, currValue + localMax) : currValue;
+        return localMax > globalMax ? localMax : globalMax;
+    }, nums[0]);
+};
+
+module.exports = maxSubArray;
