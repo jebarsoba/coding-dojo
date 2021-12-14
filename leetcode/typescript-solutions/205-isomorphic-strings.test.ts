0af395f9eefb4ae309359e988b99e578c69f3998
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2021-12-14 11:02:23 -0300

    leetcode 205, isomorphic strings

---
+++ leetcode/typescript-solutions/205-isomorphic-strings.test.ts
@@ -0,0 +1,42 @@
+test("Given two strings s and t, should determine if they are isomorphic (Example 1)", () => {
+  expect(isIsomorphic("egg", "add")).toBe(true);
+});
+
+test("Given two strings s and t, should determine if they are isomorphic (Example 2)", () => {
+  expect(isIsomorphic("foo", "bar")).toBe(false);
+});
+
+test("Given two strings s and t, should determine if they are isomorphic (Example 3)", () => {
+  expect(isIsomorphic("paper", "title")).toBe(true);
+});
+
+test("Given two strings s and t, should determine if they are isomorphic (Example 3)", () => {
+  expect(isIsomorphic("badc", "baba")).toBe(false);
+});
+
+function isIsomorphic(s: string, t: string): boolean {
+  let sArr = s.split("");
+  let tArr = t.split("");
+
+  let charMap = new Map<string, string>();
+  let mappedCharacters = new Map<string, boolean>();
+
+  for (let i = 0; i < sArr.length; i++) {
+    let mappedChar = charMap.get(sArr[i]);
+    if (mappedChar && mappedChar !== tArr[i]) {
+      return false;
+    }
+
+    if (!mappedChar) {
+      // No two characters may map to the same character...
+      if (mappedCharacters.get(tArr[i])) {
+        return false;
+      }
+
+      charMap.set(sArr[i], tArr[i]);
+      mappedCharacters.set(tArr[i], true);
+    }
+  }
+
+  return true;
+}
