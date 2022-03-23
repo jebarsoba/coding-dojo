daddaeab801108f4ffe967cbc767dffff0bfbe29
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2022-03-23 15:21:33 -0300

    refactor(morse-code-decoder): change the approach to encode and compare the context words, instead of generating all possible combinations from the morse code received

--- hackerrank/morse-code-decoder.test.js
+++ hackerrank/morse-code-decoder.test.js
@@ -1,3 +1,10 @@
+/**
+ *
+ *
+ * <TEST CASES>
+ *
+ *
+ */
 describe("input parsing", () => {
   test("parse morse code dictionary", () => {
     const morseCodeChar = parseMorseCodeChar("A   .-");
@@ -7,10 +14,18 @@ describe("input parsing", () => {
 
   test("build morse code dictionary", () => {
     const input = ["A   .-", "W   .--"];
-    expect(buildMorseCodeDictionary(input)).toEqual({
-      ".-": "A",
-      ".--": "W",
+    expect(buildTextToMorseDictionary(input)).toEqual({
+      A: ".-",
+      W: ".--",
     });
+  });
+});
+
+describe("morse code encoding", () => {
+  test("encoding example 1", () => {
+    const text = "EEE";
+    const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
+    expect(encode(text, dictionary)).toBe("...");
   });
 });
 
@@ -18,7 +33,7 @@ describe("morse code decoding", () => {
   describe("perfect match", () => {
     test("given the morse code '.--.....--' (possibly 'WHAT'), and 'WHAT' being part of the context, should return 'WHAT' as a perfect match", () => {
       const morse = ".--.....--";
-      const dictionary = buildMorseCodeDictionary(inputMorseCodeDictionary);
+      const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
       const context = ["WHAT"];
 
       expect(decode(morse, dictionary, context)).toBe("WHAT");
@@ -26,7 +41,7 @@ describe("morse code decoding", () => {
 
     test("given the morse code '.--.....--' (possibly 'WHAT'), and 'WHAT' and 'ATHAT' in the context, should return the shortest perfect match, plus an exclamation mark", () => {
       const morse = ".--.....--";
-      const dictionary = buildMorseCodeDictionary(inputMorseCodeDictionary);
+      const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
       const context = ["WHAT", "ATHAT"];
 
       expect(decode(morse, dictionary, context)).toBe("WHAT!");
@@ -36,26 +51,40 @@ describe("morse code decoding", () => {
   describe("close match", () => {
     test("given the morse code '.--.....--' (possibly 'WHAT'), and 'HAT' in the context, should return the best/closest valid word, plus a question mark and a mismatch indicator", () => {
       const morse = ".--.....--";
-      const dictionary = buildMorseCodeDictionary(inputMorseCodeDictionary);
+      const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
       const context = ["HAT"];
 
       // I don't remember exactly the rules for the computation of the mismatch indicator...
-      // I assume I have to show how many characters are different (1 in this case, comparing 'HAT' and 'WHAT').
-      expect(decode(morse, dictionary, context)).toBe("WHAT? 1");
+      // I assume I have to show how many characters are different (3 in this case, comparing the morse from 'HAT' and 'WHAT').
+      expect(decode(morse, dictionary, context)).toBe("HAT? 3");
     });
   });
 
   describe("no match", () => {
     test("given the morse code '.--.....--' (possibly 'WHAT'), without 'WHAT' being part of the context, should return that no matching was found", () => {
       const morse = ".--.....--";
-      const dictionary = buildMorseCodeDictionary(inputMorseCodeDictionary);
+      const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
       const context = [];
 
       expect(decode(morse, dictionary, context)).toBe("No matching word found");
     });
   });
 });
+/**
+ *
+ *
+ * </TEST CASES>
+ *
+ *
+ */
 
+/**
+ *
+ *
+ * <PROGRAM>
+ *
+ *
+ */
 function processData(input) {
   const { encodedWords, dictionary, context } = parseInput(input);
 
@@ -70,12 +99,20 @@ function processData(input) {
  */
 function parseInput(input) {
   // TODO: Take raw dictionary from input...
-  const dictionary = buildMorseCodeDictionary(inputMorseCodeDictionary);
+  const dictionary = buildTextToMorseDictionary(inputMorseCodeDictionary);
 
   // TODO: Parse the context and encodedWords from the real input...
   const context = ["WHAT"];
   const encodedWords = [".--.....--"];
   return { encodedWords, dictionary, context };
+}
+
+function buildTextToMorseDictionary(input) {
+  let morseCodeChars = input.map(parseMorseCodeChar);
+  return morseCodeChars.reduce((dictionary, morseCodeChar) => {
+    dictionary[morseCodeChar.character] = morseCodeChar.morse;
+    return dictionary;
+  }, {});
 }
 
 function parseMorseCodeChar(line) {
@@ -87,137 +124,94 @@ function parseMorseCodeChar(line) {
   };
 }
 
-function buildMorseCodeDictionary(input) {
-  let morseCodeChars = input.map(parseMorseCodeChar);
-  return morseCodeChars.reduce((dictionary, morseCodeChar) => {
-    dictionary[morseCodeChar.morse] = morseCodeChar.character;
-    return dictionary;
-  }, {});
-}
-
 /**
- * Decodes the morse code into all words possible,
- * and returns the word that best fits the given context (if possible)
+ * Encodes all words in the context,
+ * and returns the best one possible,
+ * depending on the ressamblance to the morse received as an input.
  */
 function decode(morse, dictionary, context) {
-  let validWords = findValidWords(morse, dictionary, []);
-
-  const perfectMatch = findPerfectMatch(validWords, context);
-  if (perfectMatch) {
-    return perfectMatch;
-  }
-
-  const closeMatch = findCloseMatch(validWords, context);
-  if (closeMatch) {
-    return closeMatch;
-  }
-
-  return "No matching word found";
-}
-
-/**
- * Returns an array of valid words that can be decoded from the given morse code
- */
-function findValidWords(morse, dictionary, alreadyFoundWords) {
-  let decodedWord = "";
-  let morseSequence = "";
-  let encodedChars = morse.split("");
-  for (let i = 0; i < encodedChars.length; i++) {
-    morseSequence += encodedChars[i];
-    const decodedChar = dictionary[morseSequence];
-
-    if (
-      decodedChar &&
-      !isAlreadyFound(decodedWord, decodedChar, alreadyFoundWords)
-    ) {
-      decodedWord += decodedChar;
-      morseSequence = "";
-    }
-  }
-
-  if (decodedWord === "") {
-    return alreadyFoundWords;
-  }
-
-  return findValidWords(morse, dictionary, [...alreadyFoundWords, decodedWord]);
-}
-
-/**
- * Checks if the decoded word will match the already found combinations
- */
-function isAlreadyFound(decodedWord, decodedChar, alreadyFoundWords) {
-  for (let alreadyFoundWord of alreadyFoundWords) {
-    if (alreadyFoundWord[decodedWord.length] === decodedChar) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function findPerfectMatch(validWords, context) {
   let perfectMatches = [];
-  for (let candidateWord of validWords) {
-    if (context.includes(candidateWord)) {
-      perfectMatches.push(candidateWord);
+  let partialMatches = [];
+
+  for (let contextWord of context) {
+    const encodedContextWord = encode(contextWord, dictionary);
+
+    if (morse === encodedContextWord) {
+      perfectMatches.push(contextWord);
+    } else if (partialMatch(morse, encodedContextWord)) {
+      partialMatches.push({
+        word: contextWord,
+        mismatch: calculateMismatchIndicator(morse, encodedContextWord),
+      });
     }
   }
 
-  if (perfectMatches.length === 0) {
-    return undefined;
+  if (perfectMatches.length === 0 && partialMatches.length === 0) {
+    return "No matching word found";
   }
 
+  if (perfectMatches.length > 0) {
+    return buildPerfectMatch(perfectMatches);
+  } else if (partialMatches.length > 0) {
+    return buildClosestMatch(partialMatches);
+  }
+}
+
+function encode(text, dictionary) {
+  return text.split("").reduce((encodedText, currChar) => {
+    encodedText += dictionary[currChar];
+    return encodedText;
+  }, "");
+}
+
+function calculateMismatchIndicator(text1, text2) {
+  return Math.abs(text1.length - text2.length).toString();
+}
+
+function partialMatch(text1, text2) {
+  const text = text1.length > text2.length ? text1 : text2;
+  const partial = text1.length > text2.length ? text2 : text1;
+  return text.toLowerCase().indexOf(partial.toLowerCase()) > -1;
+}
+
+function buildPerfectMatch(perfectMatches) {
   return (
     perfectMatches.sort((a, b) => a.length - b.length)[0] +
     (perfectMatches.length > 1 ? "!" : "")
   );
 }
 
-function findCloseMatch(validWords, context) {
-  let candidateWords = [];
-  for (let candidateWord of validWords) {
-    for (let contextWord of context) {
-      if (partialMatch(candidateWord, contextWord)) {
-        candidateWords.push({
-          word: candidateWord,
-          mismatch: calculateMismatchIndicator(candidateWord, contextWord),
-        });
-      }
-    }
-  }
-
-  if (candidateWords.length === 0) {
-    return undefined;
-  }
-
-  const closestMatch = candidateWords.sort(
+function buildClosestMatch(partialMatches) {
+  const closestMatch = partialMatches.sort(
     (a, b) => a.mismatch - b.mismatch
   )[0];
   return `${closestMatch.word}? ${closestMatch.mismatch}`;
 }
+/**
+ *
+ *
+ * </PROGRAM>
+ *
+ *
+ */
 
-function calculateMismatchIndicator(candidateWord, contextWord) {
-  return Math.abs(candidateWord.length - contextWord.length).toString();
-}
-
-function partialMatch(candidateWord, contextWord) {
-  const text =
-    candidateWord.length > contextWord.length ? candidateWord : contextWord;
-  const partial =
-    candidateWord.length > contextWord.length ? contextWord : candidateWord;
-  return text.toLowerCase().indexOf(partial.toLowerCase()) > -1;
-}
-
+/**
+ *
+ *
+ * <INPUT>
+ *
+ *
+ */
 const inputMorseCodeDictionary = [
   "A   .-",
   "B   -...",
   "C   -.-.",
   "D   -..",
-  // "E   .",
+  "E   .",
   "F   ..-.",
   "G   --.",
   "H   ....",
-  // "I   ..",
+  "I   ..",
   "J   .---",
   "K   -.-",
   "L   .-..",
@@ -227,7 +221,7 @@ const inputMorseCodeDictionary = [
   "P   .--.",
   "Q   --.-",
   "R   .-.",
-  // "S   ...",
+  "S   ...",
   "T   -",
   "U   ..-",
   "V   ...-",
@@ -246,3 +240,10 @@ const inputMorseCodeDictionary = [
   "9   ----.",
   "0   -----",
 ];
+/**
+ *
+ *
+ * </INPUT>
+ *
+ *
+ */
